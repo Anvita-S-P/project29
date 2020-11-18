9305ad6f193563d28feec4f308a7a7b586d672ce
Anvita-S-P <71773175+Anvita-S-P@users.noreply.github.com>
2020-11-18 08:46:07 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,20 @@
+class Box{
+  constructor(x,y,w,h){
+      var options ={
+          restitution:0.5
+      }
+      this.body=Bodies.rectangle(x,y,w,h,options);
+      World.add(myWorld,this.body);
+      this.width=w;
+      this.height=h;
+  }
+  display(){
+      var pos=this.body.position;
+      var angle=this.body.angle;
+      push();
+      translate(pos.x,pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0,0,this.width,this.height);
+     pop();
+  }
